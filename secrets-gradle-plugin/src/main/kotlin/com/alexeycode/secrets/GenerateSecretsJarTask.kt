f118bd8b82fa82e196404acb891c78bd8b541886
Alexey Nikitin <nikialeksey@gmail.com>
2026-03-23 22:22:17 +0400

    Mark task as cacheable one

--- secrets-gradle-plugin/src/main/kotlin/com/alexeycode/secrets/GenerateSecretsJarTask.kt
+++ secrets-gradle-plugin/src/main/kotlin/com/alexeycode/secrets/GenerateSecretsJarTask.kt
@@ -4,6 +4,7 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.Property
+import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
@@ -11,6 +12,7 @@ import java.io.FileOutputStream
 import java.util.jar.JarEntry
 import java.util.jar.JarOutputStream
 
+@CacheableTask
 abstract class GenerateSecretsJarTask : DefaultTask() {
 
     @get:Input
